c91a70b8e11843d519b17decb5be0e4e3bde62c6
Aa-thomas <Aartho95@gmail.com>
2022-04-26 11:16:44 -0400

    complete showModal functionality

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -1,9 +1,12 @@
-//fetches data frmo randomuser API
+//fetches data frmo randomdata[i] API
 function fetchData(url) {
     return fetch(url)
               .then(checkStatus)
               .then( response => response.json() )
-              .then(data => showdata(data.results ) )
+              .then(data => {
+                  generateEmployees(data.results)
+                  generateModal(data.results) 
+            })
               .catch( error => console.log('There was a problem with your request', error) );
 }
 
@@ -21,7 +24,7 @@ const userData = fetchData('https://randomuser.me/api/?results=12&nat=us');
 
 
 
-function showdata(data) {
+function generateEmployees(data) {
     for(let i = 0; i < data.length; i++) {
         const gallery = `<div class="card">
             <div class="card-img-container">
@@ -33,30 +36,48 @@ function showdata(data) {
             <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
             </div>
             </div>`
-        document.getElementById('gallery').insertAdjacentHTML('beforeend', gallery)        }
+        document.getElementById('gallery').insertAdjacentHTML('beforeend', gallery)    
+    }
+    document.querySelectorAll('div.card')
+        .forEach( card => card.addEventListener('click', (e) => showModal(e)))
 }
 
-// function showModal() {
-//     const modal = <div class="modal-container">
-//         <div class="modal">
-//         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//         <div class="modal-info-container">
-//             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-//             <h3 id="name" class="modal-name cap">name</h3>
-//             <p class="modal-text">email</p>
-//             <p class="modal-text cap">city</p>
-//             <hr>
-//             <p class="modal-text">(555) 555-5555</p>
-//             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//             <p class="modal-text">Birthday: 10/21/2015</p>
-//         </div>
-//         </div>
-// }
+function generateModal(data) {
+    for(let i = 0; i < data.length; i++) {
+    const modal = `<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+            <p class="modal-text">${data[i].email}</p>
+            <p class="modal-text cap">${data[i].location.city}</p>
+            <hr>
+            <p class="modal-text">${data[i].phone}</p>
+            <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+            <p class="modal-text">Birthday: birthday</p>
+        </div>
+    </div>`;
+    document.querySelector('body').insertAdjacentHTML('beforeend', modal);
+    }
+    document.querySelectorAll('#modal-close-btn')
+        .forEach( button => button.addEventListener('click', () => closeModal() ) )
+    
+    closeModal()
+    
+}
+function index(e) {
+    let cards = Array.from(document.querySelectorAll('div.card'))
+    return cards.indexOf(e.target.closest('div.card'))
+}
 
-function fetchModalData(url) {
-         fetch(url)
-              .then(checkStatus)
-              .then( response => response.json() )
-       return .then(data => data.results ) 
-              .catch( error => console.log('There was a problem with your request', error) );
+function showModal(e) {
+   let card =  document.querySelectorAll('div.modal-container')[index(e)];
+   card.style.display = 'block'
+   
+}
+
+function closeModal() {
+    document.querySelectorAll('div.modal-container')
+        .forEach( modal => modal.style.display = 'none');
 }
