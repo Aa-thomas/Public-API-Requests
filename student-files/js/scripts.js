adebe69cfe217aa9eccdbaf39f3aad621666b5b4
Aa-thomas <aarthomas01@gmail.com>
2023-01-19 18:37:04 -0500

    fix: Add formatted date to modal

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -4,20 +4,25 @@
  * @returns - A Promise object containing data from randomUserAPI that is used to generate employee information
  */
 function getRandomUsers(randomUserAPI) {
-    return fetch(randomUserAPI)
-            .then(checkStatus)
-            .then( response => response.json() )
-            .then(data => {
-                generateEmployees(data.results);
-                generateModal(data.results);
-                generateSearchBar(data.results); 
-            })
-            .catch( error => console.log('There was a problem with your request', error) );
+	return fetch(randomUserAPI)
+		.then((response) => checkStatus(response))
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			generateEmployees(data.results);
+			generateModal(data.results);
+			generateSearchBar(data.results);
+		})
+		.catch((error) =>
+			console.log('There was a problem with your request', error)
+		);
 }
 
 //checks status and handles errors
 function checkStatus(response) {
-return response.ok ?  Promise.resolve(response) : Promise.reject( new Error(response.statusText) )
+	return response.ok
+		? Promise.resolve(response)
+		: Promise.reject(new Error(response.statusText));
 }
 
 getRandomUsers('https://randomuser.me/api/?results=12&nat=us');
@@ -27,8 +32,8 @@ getRandomUsers('https://randomuser.me/api/?results=12&nat=us');
  * @param {Array} data - An array of objects containing employee information
  */
 function generateEmployees(data) {
-    for(let i = 0; i < data.length; i++) {
-        const gallery = `<div class="card">
+	for (let i = 0; i < data.length; i++) {
+		const gallery = `<div class="card">
             <div class="card-img-container">
             <img class="card-img" src="${data[i].picture.large}" alt="profile picture">
             </div>
@@ -37,20 +42,33 @@ function generateEmployees(data) {
             <p class="card-text">${data[i].email}</p>
             <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
             </div>
-            </div>`
-        document.getElementById('gallery').insertAdjacentHTML('beforeend', gallery)    
-    }
-    document.querySelectorAll('div.card')
-        .forEach( card => card.addEventListener('click', (event) => showModalOnClick(event)))
+            </div>`;
+		document
+			.getElementById('gallery')
+			.insertAdjacentHTML('beforeend', gallery);
+	}
+	document
+		.querySelectorAll('div.card')
+		.forEach((card) =>
+			card.addEventListener('click', (event) => showModalOnClick(event))
+		);
 }
 
 /**
- * Generates Modal for each employee and adds event listeners 
+ * Generates Modal for each employee and adds event listeners
  * @param {Array} data - An array of objects containing employee information
  */
 function generateModal(data) {
-    for(let i = 0; i < data.length; i++) {
-        const modal = `<div class="modal-container">
+	for (let i = 0; i < data.length; i++) {
+		const birthdayTimestamp = data[i].dob.date;
+		const birthday = new Date(birthdayTimestamp);
+		const birthdayFormatted = birthday.toLocaleDateString('en-US', {
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric',
+		});
+		console.log(birthdayFormatted);
+		const modal = `<div class="modal-container">
             <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
@@ -61,7 +79,7 @@ function generateModal(data) {
             <hr>
             <p class="modal-text">${data[i].phone}</p>
             <p class="modal-text">${data[i].location.street.number} ${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
-            <p class="modal-text">Birthday: birthday</p>
+            <p class="modal-text">Birthday: ${birthdayFormatted}</p>
             </div>
             </div>
             <div class="modal-btn-container">
@@ -69,63 +87,69 @@ function generateModal(data) {
             <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
             </div>`;
-        document.querySelector('body').insertAdjacentHTML('beforeend', modal);
-    }
-    // Event listener: Close Modal Button
-    document.querySelectorAll('#modal-close-btn')
-        .forEach( button => button.addEventListener('click', () => closeActiveModal() ) );
-    
-    // Event listener: Next Modal Button
-    document.querySelectorAll('#modal-next')
-       .forEach( button => {button.addEventListener('click', () => nextModal() ) });
+		document.querySelector('body').insertAdjacentHTML('beforeend', modal);
+	}
+	// Event listener: Close Modal Button
+	document
+		.querySelectorAll('#modal-close-btn')
+		.forEach((button) =>
+			button.addEventListener('click', () => closeActiveModal())
+		);
 
-     // Event listener: Previous Modal Button
-    document.querySelectorAll('#modal-prev')
-       .forEach( button => {button.addEventListener('click', () => prevModal() ) });
+	// Event listener: Next Modal Button
+	document.querySelectorAll('#modal-next').forEach((button) => {
+		button.addEventListener('click', () => next());
+	});
 
-     // Event listener: Arrow Keys 
-    document.addEventListener('keyup', (e) => {
-        if (document.getElementById('active-modal') !== null) {
-            if (e.key === "ArrowRight") {
-                nextModal()
-            }
-            if (e.key === "ArrowLeft") {
-                prevModal()
-            }
-        }
-    })
-    
-    //Hide all modals
-    document.querySelectorAll('div.modal-container')
-       .forEach( modal => modal.style.display = 'none');
-    
+	// Event listener: Previous Modal Button
+	document.querySelectorAll('#modal-prev').forEach((button) => {
+		button.addEventListener('click', () => prev());
+	});
+
+	// Event listener: Arrow Keys
+	document.addEventListener('keyup', (e) => {
+		if (document.getElementById('active-modal') !== null) {
+			if (e.key === 'ArrowRight') {
+				next();
+			}
+			if (e.key === 'ArrowLeft') {
+				prev();
+			}
+		}
+	});
+
+	//Hide all modals
+	document
+		.querySelectorAll('div.modal-container')
+		.forEach((modal) => (modal.style.display = 'none'));
 }
 
 /**
- * Generates a searchbar 
+ * Generates a searchbar
  * @param {*} data - An array of objects containing employee information
  */
 function generateSearchBar(data) {
-    const searchBar = `<form action="#" method="get">
+	const searchBar = `<form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
         </form>`;
-    document.querySelector('div.search-container').insertAdjacentHTML('beforeend', searchBar);
-   
-    // Event Listener: Search Button
-    const searchButton = document.getElementById('search-submit');
-    searchButton.addEventListener('click', () => {
-        searchFilter(data, searchInput.value );
-        searchInput.value = '';
-     });
-    
-     // Event Listener: Search Input
-    let searchInput = document.getElementById('search-input');
-     searchInput.addEventListener('keyup', () => {
-        searchFilter(data, searchInput.value);
-    });
-}
+	document
+		.querySelector('div.search-container')
+		.insertAdjacentHTML('beforeend', searchBar);
 
+	// Event Listener: Search Button
+	const searchButton = document.getElementById('search-submit');
+	searchButton.addEventListener('click', () => {
+		searchFilter(data, searchInput.value);
+		searchInput.value = '';
+	});
+
+	// Event Listener: Search Input
+	let searchInput = document.getElementById('search-input');
+	searchInput.addEventListener('keyup', () => {
+		searchFilter(data, searchInput.value);
+	});
+}
 
 /**
  * Employee search function
@@ -133,72 +157,81 @@ function generateSearchBar(data) {
  * @param {string} searchQuery - The search term
  */
 function searchFilter(employeeList, searchQuery) {
-    const searchResults = [];
-    searchQuery = searchQuery.toLowerCase();
-    
-    document.getElementById('gallery').innerHTML = '';
-    document.querySelectorAll('div.modal-container')
-        .forEach( modal => modal.remove() );
+	const searchResults = [];
+	searchQuery = searchQuery.toLowerCase();
 
-    employeeList.forEach( employee =>  {
-        let employeeFullName = `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`;
-        if (employeeFullName.includes(searchQuery)) {
-            searchResults.push(employee);
-        }
-    });
-    generateEmployees(searchResults);
-    generateModal(searchResults);
+	document.getElementById('gallery').innerHTML = '';
+	document
+		.querySelectorAll('div.modal-container')
+		.forEach((modal) => modal.remove());
+
+	employeeList.forEach((employee) => {
+		const employeeFullName = `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`;
+		if (employeeFullName.includes(searchQuery)) {
+			searchResults.push(employee);
+		}
+	});
+	generateEmployees(searchResults);
+	generateModal(searchResults);
 }
 
 /**
  * Modal Navigation and manipulation
  */
 
-// Returns the index of employee card when clicked   
+// Returns the index of employee card when clicked
 function getCardIndex(e) {
-    const  employeeCardArray = Array.from(document.querySelectorAll('div.card'));
-    const employeeCardIndex = employeeCardArray.indexOf(e.target.closest('div.card'));
-    return employeeCardIndex;
+	const employeeCardArray = Array.from(document.querySelectorAll('div.card'));
+	const cardIndex = employeeCardArray.indexOf(e.target.closest('div.card'));
+	return cardIndex;
 }
 
 // Returns index of the modal that is currently being displayed
 function getModalIndex() {
-    const  modalArray = Array.from(document.querySelectorAll('div.modal-container'));
-    const modalIndex = modalArray.indexOf(document.getElementById('active-modal'));
-    return modalIndex;
+	const modalArray = Array.from(
+		document.querySelectorAll('div.modal-container')
+	);
+	const modalIndex = modalArray.indexOf(
+		document.getElementById('active-modal')
+	);
+	return modalIndex;
 }
 
 // Display modal window and set id attribute
 function showModal(modal) {
-    modal.style.display = 'block';
-    modal.setAttribute('id', 'active-modal' );
+	modal.style.display = 'block';
+	modal.setAttribute('id', 'active-modal');
 }
 
 // Show modal when clicked
 function showModalOnClick(e) {
-   const modal =  document.querySelectorAll('div.modal-container')[getCardIndex(e)];
-   showModal(modal);   
+	const modal = document.querySelectorAll('div.modal-container')[
+		getCardIndex(e)
+	];
+	showModal(modal);
 }
 
 // Close Modal and remove id attribute
 function closeActiveModal() {
-    const activeModal = document.getElementById('active-modal');
-    activeModal.style.display = 'none';
-    activeModal.removeAttribute('id');       
+	const activeModal = document.getElementById('active-modal');
+	activeModal.style.display = 'none';
+	activeModal.removeAttribute('id');
 }
 
 // Display the next modal
 function nextModal() {
-    const nextModal =  document.querySelectorAll('div.modal-container')[getModalIndex() + 1];
-    closeActiveModal();
-    showModal(nextModal);
+	const next = document.querySelectorAll('div.modal-container')[
+		getModalIndex() + 1
+	];
+	closeActiveModal();
+	showModal(next);
 }
 
 // Display the previous modal
 function prevModal() {
-    const prevModal =  document.querySelectorAll('div.modal-container')[getModalIndex() - 1];
-    closeActiveModal();
-    showModal(prevModal);
+	const prev = document.querySelectorAll('div.modal-container')[
+		getModalIndex() - 1
+	];
+	closeActiveModal();
+	showModal(prev);
 }
-
-
